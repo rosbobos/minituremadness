20df3b97a1dbc9f2b07bd1d3a9528113c16e1eb2
rosbobos <53958255+rosbobos@users.noreply.github.com>
2020-04-21 16:34:27 -0700

    Got the basic CRUD started

--- MiniatureMadness/MiniatureMadness/Controllers/HomeController.cs
+++ MiniatureMadness/MiniatureMadness/Controllers/HomeController.cs
@@ -3,6 +3,12 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Http;
+using Microsoft.EntityFrameworkCore;
+using MiniatureMadness.Models;
+using MiniatureMadness.Data;
+using MiniatureMadness.Models.Interfaces;
+
 
 namespace MiniatureMadness.Controllers
 {
@@ -11,6 +17,42 @@ namespace MiniatureMadness.Controllers
         public IActionResult Index()
         {
             return View();
+        }
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<Products>>> GetPoroducts() => await _context.GetAllProducts();
+
+        [HttpGet("{id}")]
+        public async Task<ActionResult<Products>> GetProducts(int id)
+        {
+            var products = await _context.GetProducts(id);
+
+
+            if (products == null)
+            {
+                return NotFound();
+            }
+
+            return products;
+        }
+
+        [HttpPut("{id}")]
+        public async Task PutProducts(int id, Products products)
+        {
+            await _context.UpdateProducts(products);
+        }
+
+        [HttpPost]
+        public async Task<ActionResult<Products>> PostProducts(Products products)
+        {
+            var product = await _context.CreateProduct(product);
+
+            return CreatedAtAction("GetProduct", new { id = product.ID }, product);
+        }
+
+        [HttpDelete("{id}")]
+        public async Task DeleteProduct(int id)
+        {
+            await _context.DeleteProduct(id);
         }
     }
 }
