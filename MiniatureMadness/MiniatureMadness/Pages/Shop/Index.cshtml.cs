8683b46feafa1faaac27598422219347d11cb357
Robert James Nielsen <robert.j.nielsen@outlook.com>
2020-04-23 13:54:20 -0700

    Added Shop/Index Razor Page.

---
+++ MiniatureMadness/MiniatureMadness/Pages/Shop/Index.cshtml.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using MiniatureMadness.Data;
+
+namespace MiniatureMadness.Pages.Shop
+{
+    public class IndexModel : PageModel
+    {
+        private StoreDBContext _context;
+
+        public IndexModel(StoreDBContext storeDBContext)
+        {
+            _context = storeDBContext;
+        }
+
+        public void OnGet()
+        {
+            
+        }
+    }
+}
