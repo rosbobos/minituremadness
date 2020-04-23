40e8c03d44f1108e4df8fa50a1c473fae312bed8
Robert James Nielsen <robert.j.nielsen@outlook.com>
2020-04-23 14:28:12 -0700

    Product info renders on Shop/Index Page.

--- MiniatureMadness/MiniatureMadness/Pages/Shop/Index.cshtml.cs
+++ MiniatureMadness/MiniatureMadness/Pages/Shop/Index.cshtml.cs
@@ -5,21 +5,27 @@ using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 using MiniatureMadness.Data;
+using MiniatureMadness.Models;
+using MiniatureMadness.Models.Interfaces;
 
 namespace MiniatureMadness.Pages.Shop
 {
     public class IndexModel : PageModel
     {
-        private StoreDBContext _context;
+        private IInventory _inventory;
 
-        public IndexModel(StoreDBContext storeDBContext)
+        public List<Product> Products { get; set; }
+
+        public IndexModel(IInventory inventory)
         {
-            _context = storeDBContext;
+            _inventory = inventory;
         }
 
         public void OnGet()
         {
-            
+            var result = _inventory.GetAllProducts();
+
+            Products = result;
         }
     }
 }
