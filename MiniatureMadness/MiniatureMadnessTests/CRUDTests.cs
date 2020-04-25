089a89cfa20de335e30bbd63c07cb778fbde470e
Robert Nielsen <robert.j.nielsen@outlook.com>
2020-04-25 13:43:30 -0700

    CRUDTests file created.

---
+++ MiniatureMadness/MiniatureMadnessTests/CRUDTests.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using MiniatureMadness.Models;
+using MiniatureMadness.Models.Services;
+using MiniatureMadness.Data;
+using Microsoft.EntityFrameworkCore;
+
+namespace MiniatureMadnessTests
+{
+    class CRUDTests
+    {
+    }
+}
