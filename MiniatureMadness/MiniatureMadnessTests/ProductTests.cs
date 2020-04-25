d9c71a5dcba515cdb009de5f50301782279b1e55
Robert Nielsen <robert.j.nielsen@outlook.com>
2020-04-25 13:32:35 -0700

    Test CanAddSkuToProducts passed.

--- MiniatureMadness/MiniatureMadnessTests/ProductTests.cs
+++ MiniatureMadness/MiniatureMadnessTests/ProductTests.cs
@@ -1,12 +1,24 @@
 using System;
 using Xunit;
+using MiniatureMadness.Models;
 
 namespace MiniatureMadnessTests
 {
-    public class UnitTest1
+    public class ProductTests
     {
         [Fact]
-        public void Test1()
+        public void CanAddSkuToProducts()
+        {
+            Product product = new Product();
+            product.Sku = "155-837";
+
+            string expected = "155-837";
+
+            Assert.Equal(expected, product.Sku);
+        }
+
+        [Fact]
+        public void CanAddNameToProducts()
         {
 
         }
