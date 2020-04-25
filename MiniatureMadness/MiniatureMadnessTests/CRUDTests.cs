e0708c853d22f3ba66a1c7f4fcdd6bac3c7cb482
Robert Nielsen <robert.j.nielsen@outlook.com>
2020-04-25 15:01:37 -0700

    Test CanUpdateAProductInTheDatabase passed.

--- MiniatureMadness/MiniatureMadnessTests/CRUDTests.cs
+++ MiniatureMadness/MiniatureMadnessTests/CRUDTests.cs
@@ -103,5 +103,92 @@ namespace MiniatureMadnessTests
                 Assert.Equal(expected, data);
             }
         }
+
+        /// <summary>
+        /// Tests that we can retrieve a single Product from the database.
+        /// </summary>
+        [Fact]
+        public async void CanGetASingleProductFromDatabase()
+        {
+            // Instantiates our InMemory database with our StoreDBContext.
+            DbContextOptions<StoreDBContext> options = new DbContextOptionsBuilder<StoreDBContext>()
+                .UseInMemoryDatabase("CanGetASingleProductFromDatabase")
+                .Options;
+
+            // Uses our InMemory database.
+            using (StoreDBContext context = new StoreDBContext(options))
+            {
+                // "Grabs" our InventoryService.
+                InventoryService service = new InventoryService(context);
+
+                // Define new Products to be inserted into the database.
+                Product product = new Product()
+                {
+                    Sku = "123-456",
+                    Name = "Super Doggo",
+                    Description = "Super Doggo is a loyal, brave, and ferocious crime fighter! He'll also fetch you the newspaper...",
+                    Price = 129.95M,
+                    Image = "/Assets/Images/Products/super-doggo.png"
+                };
+
+                // Inserts the Product into the database.
+                // Product should have an ID of 1, as it is the only Product in the database.
+                var result = await service.CreateAProduct(product);
+
+                Product expected = product;
+
+                // Retrieve all Products from the database.
+                var data = await service.GetAProduct(result.ID);
+
+                Assert.Equal(expected, data);
+            }
+        }
+
+        /// <summary>
+        /// Tests that we can update an existing Product in the database.
+        /// </summary>
+        [Fact]
+        public async void CanUpdateAProductInTheDatabase()
+        {
+            // Instantiates our InMemory database with our StoreDBContext.
+            DbContextOptions<StoreDBContext> options = new DbContextOptionsBuilder<StoreDBContext>()
+                .UseInMemoryDatabase("CanGetASingleProductFromDatabase")
+                .Options;
+
+            // Uses our InMemory database.
+            using (StoreDBContext context = new StoreDBContext(options))
+            {
+                // "Grabs" our InventoryService.
+                InventoryService service = new InventoryService(context);
+
+                // Define new Products to be inserted into the database.
+                Product product = new Product()
+                {
+                    Sku = "123-456",
+                    Name = "Super Doggo",
+                    Description = "Super Doggo is a loyal, brave, and ferocious crime fighter! He'll also fetch you the newspaper...",
+                    Price = 129.95M,
+                    Image = "/Assets/Images/Products/super-doggo.png"
+                };
+
+                // Inserts the Product into the database.
+                var result = await service.CreateAProduct(product);
+
+                // Update the product Price.
+                result.Price = 49.99M;
+
+                // Determine that our expected value will be the updated Price.
+                decimal expected = 49.99M;
+
+                // Update our Product.
+                await service.UpdateAProduct(result);
+
+                // Retrieve our updated Product from the database.
+                var data = service.GetAProduct(result.ID);
+                var updatedProduct = data.Result;
+
+                Assert.Equal(expected, updatedProduct.Price);
+            }
+        }
     }
 }
